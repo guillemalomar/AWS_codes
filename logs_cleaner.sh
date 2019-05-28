21b16a4bc11e22a5b1a5c50ebc316a0f64f48ed4
Guillem Nicolau Alomar Sitjes <guillemnicolau@gmail.com>
2019-05-28 16:31:52 +0800

    Update logs_cleaner.sh

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -3,46 +3,66 @@
 # This script is used to delete logs from a Kibana endpoint.
 # You might need to change not only the endpoint of your Kibana server, but also the format of the dates
 
-ENDPOINT="https://stack.region.es.amazonaws.com/project_logs-"
+ENDPOINT="https://XXXXX.XXXXX.es.amazonaws.com/project_logs-"
 LIMIT_DAYS=10
-DAYS_TO_DELETE=14
 FIRST_DAY=$((LIMIT_DAYS+1))
 CURR_DATE=$(date +'%Y.%m.%d')
 
-if [[ "$#" -eq 0 ]]; then  # If there are no parameters
+delete_date_range () {
   DAYS=`expr ${DAYS_TO_DELETE} + ${LIMIT_DAYS}`
   for (( i=FIRST_DAY; i<=$DAYS; i++ )); do
-    PREV_DATE=$(date -j -v-"$i"d -f "%Y.%m.%d" ${CURR_DATE} "+%Y.%m.%d")
-    echo Deleting file ${PREV_DATE}
-    curl -X DELETE "$ENDPOINT""$PREV_DATE"
-    echo '\n'
+    DATE_TO_DEL=$(date -j -v-"$i"d -f "%Y.%m.%d" ${CURR_DATE} "+%Y.%m.%d")
+    delete_file
   done
-elif [[ "$#" -eq 1 ]]; then  # If there are parameters
+}
+
+past_date () {
+  if [[ ${DATE_TO_DEL} > ${CURR_DATE} ]]; then
+    echo false
+  else
+    echo true
+  fi
+}
+
+old_enough () {
+  for (( i=0; i<=$LIMIT_DAYS; i++ )); do
+    PREV_DATE=$(date -j -v-"$i"d -f "%Y.%m.%d" ${CURR_DATE} "+%Y.%m.%d")
+    if [[ "${DATE_TO_DEL}" = "${PREV_DATE}" ]]; then
+      echo false
+      return
+    fi
+  done
+  echo true
+}
+
+delete_file () {
+  echo Deleting file ${DATE_TO_DEL}
+  curl -X DELETE "$ENDPOINT""$DATE_TO_DEL"
+  echo '\n'
+}
+
+if [[ "$#" -eq 0 ]]; then                                    # If there are no parameters
+  DAYS_TO_DELETE=14
+  delete_date_range
+elif [[ "$#" -eq 1 ]]; then                                  # If there are parameters
   if [[ -n "$1" ]] && [[ "$1" -eq "$1" ]] 2>/dev/null; then  # If the parameter is an integer
-    DAYS=`expr $1 + ${LIMIT_DAYS}`
-    for (( i=$FIRST_DAY; i<=$DAYS; i++ )); do
-      PREV_DATE=$(date -j -v-"$i"d -f "%Y.%m.%d" ${CURR_DATE} "+%Y.%m.%d")
-      echo Deleting file ${PREV_DATE}
-      curl -X DELETE "$ENDPOINT""$PREV_DATE"
-      echo '\n'
-    done
-  else  # If the parameter is a string
-    if [[ $1 > ${CURR_DATE} ]]; then
+    DAYS_TO_DELETE=$1
+    delete_date_range
+  else                                                       # If the parameter is a string
+    DATE_TO_DEL=$1
+    IS_PAST_DATE="$(past_date)"
+    if [[ ${IS_PAST_DATE} = "false" ]]; then
       echo The provided day is from the future.
       exit 1
     fi
-    for (( i=0; i<=$LIMIT_DAYS; i++ )); do
-      PREV_DATE=$(date -j -v-"$i"d -f "%Y.%m.%d" ${CURR_DATE} "+%Y.%m.%d")
-      if [[ "$1" = "$PREV_DATE" ]]; then
-        echo The provided day is too recent. No logs from the last 10 days can be deleted.
-        exit 1
-      fi
-    done
-    echo Deleting file ${1}
-    curl -X DELETE "$ENDPOINT""$1"
-    echo '\n'
+    IS_OLD_ENOUGH="$(old_enough)"
+    if [[ ${IS_OLD_ENOUGH} = "false" ]]; then
+      echo The provided day is too recent. No logs from the last 10 days can be deleted.
+      exit 1
+    fi
+    delete_file
   fi
 elif [[ "$#" -ge 2 ]]; then
   echo Wrong input. It should be:
-  echo kibana_old_logs_cleaner.sh \(date in YYYY.MM.DD format/amount of days to delete\)
+  echo logs_cleaner.sh \(date in YYYY.MM.DD format / amount of days to delete\)
 fi
